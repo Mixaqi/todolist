5ac8df784ac414942094c8510fd5381c8f38d402
Mixaqi <mixaqimixaqi@mail.ru>
2023-10-01 19:21:19 +0300

    added logic for delete, update

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -2,7 +2,8 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods
 
 from .models import ToDo
 
@@ -17,3 +18,23 @@ def index(request: HttpRequest) -> HttpResponse:
         "todoapp/index.html",
         {"todo_list": todos, "title": "Главная страница"},
     )
+
+
+@require_http_methods(["POST"])
+def add(request: HttpRequest) -> HttpResponse:
+    title = request.POST["title"]
+    todo = ToDo(title=title)
+    todo.save()
+    return redirect("index")
+
+
+def update(request: HttpRequest, todo_id: int) -> HttpResponse:
+    todo = ToDo.objects.get(id=todo_id)
+    todo.is_complete = not todo.is_complete
+    todo.save()
+    return redirect("index")
+
+def delete(request: HttpRequest, todo_id: int) -> HttpResponse:
+    todo = ToDo.objects.get(id = todo_id)
+    todo.delete()
+    return redirect("index")
