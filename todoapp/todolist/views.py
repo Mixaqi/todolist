2b8f5f32372dd496e5588033e4fc0c237545b713
Mixaqi <mixaqimixaqi@mail.ru>
2023-10-01 19:09:30 +0300

    fixed html-files

--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,7 +1,19 @@
 from __future__ import annotations
 
-from django.http import HttpRequest, HttpResponse
+from typing import TYPE_CHECKING
+
+from django.shortcuts import render
+
+from .models import ToDo
+
+if TYPE_CHECKING:
+    from django.http import HttpRequest, HttpResponse
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hello World")
+    todos = ToDo.objects.all()
+    return render(
+        request,
+        "todoapp/index.html",
+        {"todo_list": todos, "title": "Главная страница"},
+    )
