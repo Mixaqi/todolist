b7406a3290af997408500010971c69d35568c499
Mixaqi <mixaqimixaqi@mail.ru>
2023-10-24 19:50:33 +0300

    need to test new orm model and graph imagination

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -6,8 +6,7 @@ from django.db import models
 class ToDo(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField("Название задания", max_length=500)
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    attached_files = models.ManyToManyField('AttachedFile')
+    user_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True, blank=True)
 
     class Meta:
         verbose_name = "Задание"
@@ -18,17 +17,17 @@ class ToDo(models.Model):
 
 class Project(models.Model):
     id = models.AutoField(primary_key=True)
-    task_id = models.ForeignKey('Task', on_delete=models.CASCADE)
+    task_id = models.ForeignKey('ToDo', on_delete=models.CASCADE)
     user_id = models.ForeignKey('User', on_delete=models.CASCADE)
 
 class User(models.Model):
     uuid = models.CharField(max_length=255, primary_key=True)
     username = models.CharField(max_length=255)
     email = models.EmailField()
-    task_id = models.IntegerField()
+    task_id = models.ForeignKey('ToDo', on_delete=models.SET_NULL, null=True, blank=True)
 
 class AttachedFile(models.Model):
     id = models.AutoField(primary_key=True)
-    loader = models.CharField(max_length=255)
+    loader = models.ForeignKey('User', on_delete=models.CASCADE)
     type = models.CharField(max_length=255)
-    task_id = models.ForeignKey('Task', on_delete=models.CASCADE)
+    task_id = models.ForeignKey('ToDo', on_delete=models.CASCADE)
